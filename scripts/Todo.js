721346da5001c7c887370ad288b861302b2998c0
Lê Bá Đích <70245065+dickdzaii@users.noreply.github.com>
2021-12-05 22:17:23 +0700

    update bulk 20211205

--- scripts/Todo.js
+++ scripts/Todo.js
@@ -5,6 +5,7 @@ class Todo {
         this.dueDate = dueDate;
         this.priority = priority;
         this.isDone = false;
+        this.created = new Date();
     }
 
 }
@@ -50,7 +51,6 @@ class ListTodo {
             let index = this.list.indexOf(foundItem);
             this.list.splice(index, 1);
             saveToLocalStorage(this.listName, this.list);
-            LoadPage(currentPage)
         } else throw new Error("Could not find task has id " + id)
     }
 
@@ -60,18 +60,17 @@ class ListTodo {
             let index = this.list.indexOf(foundItem);
             this.list.splice(index, 1);
             saveToLocalStorage(this.listName, this.list);
-            LoadPage(currentPage)
         } else throw new Error("Could not find task has id " + id)
-
     }
 
     showAllTasks() {
-        return this.list.map(e => `<div class="single-task d-flex justify-content-between ${e.isDone? 'success':e.priority}-task border px-2 py-3 my-1">
-        <div>
-        <input type="checkbox" class="task-status" name="done-task-${e.id}" id="done-${e.id}" ${e.isDone? 'checked':''} class="">
-       <span class="font-weight-bold">${e.name}</span>
+        return this.list.map((e, i) => `<div class="single-task d-flex justify-content-between ${e.isDone? 'success':e.priority}-task border px-2 py-3 my-1">
+        <div class="task-name">
+        <input type="checkbox" class="task-status" name="done-task-${e.id}" id="done-${e.id}" class="">
+       <span class="font-weight-bold">${++i}. ${e.name}</span>
         </div>
        <div>
+       <button class="btn btn-success task-success" name="success-${e.id}" id="success-${e.id}">${e.isDone?'Undone':'Done'}</button>
        <button class="btn btn-info task-detail" name="detail-${e.id}" id="detail-${e.id}">Detail</button>
        <button class="btn btn-danger delete-task" id="delete-${e.id}">Remove</button>
        </div>
--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,8 @@ function LoadPage(pageName, ...params) {
                     </div>
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-success form-control" id="addTask">${params.length>0?'Update':'Add'}</button>
+                    <button class="btn btn-success" id="addTask">${params.length>0?'Update':'Add'}</button>
+                    <button class="btn btn-primary" id="cancelForm">Cancel</button>
                 </div>
                 <div><a href="" onclick="LoadPage('tasklist')">my task</a></div>
                 </div>`;
@@ -44,6 +45,8 @@ function LoadPage(pageName, ...params) {
                 const descriptionField = document.querySelector('textarea');
                 const dueDateField = document.querySelector('input[type="date"]');
                 const priorityField = document.querySelector('select');
+                const cancel = document.getElementById("cancelForm");
+
                 let today = new Date();
                 let month = today.getMonth() < 9 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
                 let date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate;
@@ -54,9 +57,21 @@ function LoadPage(pageName, ...params) {
                     let task = listTasks.findItemById(id);
                     if (task) {
                         nameField.value = task.name;
+
                         descriptionField.value = task.description;
+
                         dueDateField.value = task.dueDate;
+
                         priorityField.value = task.priority;
+                        if (task.isDone) {
+                            nameField.setAttribute("readonly", true);
+
+                            descriptionField.setAttribute("readonly", true);
+
+                            dueDateField.setAttribute("readonly", true);
+
+                            priorityField.setAttribute("readonly", true);
+                        }
                     } else throw new Error('Task not found');
                 } else {
                     nameField.value = '';
@@ -76,6 +91,8 @@ function LoadPage(pageName, ...params) {
                     } else throw new Error('task name must be specified')
                 }
                 addButton.addEventListener('click', function() { addOrUpdateTask() })
+                cancel.addEventListener('click', function() { LoadPage(myTask) })
+
                 break;
             }
         case myTask:
@@ -84,27 +101,97 @@ function LoadPage(pageName, ...params) {
                 main.innerHTML = `<div class="text-center">
                 <h1>My Tasks</h1>
                 </div>
-                <div><button class="btn btn-primary" id="create-new">Create task</button></div>
-                ` + (listTasks.list.length > 0 ? `<div class="item-show my-3">
+                <div class="d-flex my-3">
+                <div>
+                <button class="btn btn-primary" id="create-new">Create task</button>
                 <span>Show</span> <input type="number" min=1 value=10>
                 <span>entries</span>
                 </div>
-                <div class="task-list">
+                <div id="bulk-area" class="mx-2 hidden">
+                <button type="button" id="change-status-selected" class="btn btn-success">Done/Undone</button>
+                <button type="button" class="btn btn-danger">Remove</button>
+                </div>
+                </div>
+                ` + (listTasks.list.length > 0 ?
+                    `<div class="task-list">
+                    <div class="single-task d-flex justify-content-between  border px-2 py-3 my-1">
+                    <div>
+                    <input type="checkbox" class="check-all" name="check-all" id="check-all" class="">
+                    <span class="font-weight-bold"></span>
+                    </div>
+                    <div>
+                    </div>
+                    </div>
                 ${listTasks.showAllTasks()}              
                 </div>` : `\<p class="my-2 text-center">You don't have any tasks. Enjoy you day!</p>`);
                 const createNewButton = document.getElementById("create-new");
                 createNewButton.addEventListener("click", () => { LoadPage(taskForm); });
                 const deleteButtons = document.querySelectorAll(".delete-task");
                 const taskCheckbox = document.querySelectorAll(".task-status");
-                const detailButtons = document.querySelectorAll(".task-detail")
+                const detailButtons = document.querySelectorAll(".task-detail");
+                const doneButtons = document.querySelectorAll(".task-success");
+                const bulkArea = document.getElementById("bulk-area");
+                const doneButton = document.getElementById("change-status-selected");
+                const checkAllBox = document.getElementById('check-all')
+                checkAllBox.addEventListener("click", function() {
+                    let allChecked = true;
+                    for (const i of taskCheckbox) {
+                        if (!i.checked) {
+                            allChecked = false;
+                            break;
+                        }
+                    }
+                    if (allChecked) {
+                        for (const i of taskCheckbox) {
+                            i.checked = false;
+                        }
+                    } else {
+                        for (const i of taskCheckbox) {
+                            i.checked = true;
+                        }
+                    }
+                    showBulkArea();
+                })
                 for (const e of deleteButtons) {
                     e.addEventListener("click", function() { deleteTask(e.id.split('-')[1]) })
                 }
+
+                for (const e of doneButtons) {
+                    e.addEventListener("click", function() { changeStatus([e.id.split('-')[1]]) })
+                }
+
                 for (const e of detailButtons) {
                     e.addEventListener("click", function() { LoadPage(taskForm, e.id.split('-')[1]) })
                 }
+
                 for (const e of taskCheckbox) {
-                    e.addEventListener("change", function() { changeStatus(e.id.split('-')[1]) })
+                    e.addEventListener("change", function() {
+                        if (!e.check) { checkAllBox.checked = false; }
+                        showBulkArea()
+                    })
+                }
+
+                doneButton.addEventListener("click", function() {
+                    if (confirm("Are you sure you want to done/undone these selected tasks")) {
+                        let listSelected = [];
+                        for (const i of taskCheckbox) {
+                            if (i.checked)
+                                listSelected.push(i.id.split('-')[1]);
+                        }
+                        let rs = changeStatus(listSelected);
+                        rs ? LoadPage(currentPage) : console.error("Couldn't change tasks's status");
+                    }
+                })
+
+                function showBulkArea() {
+                    for (let i of taskCheckbox) {
+                        if (i.checked) {
+                            bulkArea.className = "mx-2";
+                            return;
+                        }
+                    }
+                    bulkArea.className = "mx-2 hidden";
+                    checkAllBox.checked = false;
                 }
 
                 function deleteTask(taskId) {
@@ -112,16 +199,23 @@ function LoadPage(pageName, ...params) {
                     if (task) {
                         if (confirm('Delete task?')) {
                             listTasks.deleteItem(taskId);
+                            LoadPage(currentPage);
                         }
                     } else throw new Error('Task not found');
                 }
 
-                function changeStatus(taskId) {
-                    let task = listTasks.findItemById(taskId);
-                    if (task) {
-                        task.isDone = !task.isDone;
-                        listTasks.updateItem(taskId, task)
-                    } else throw new Error(`Task has id ${taskId} not found`);
+                function changeStatus(tasks) {
+                    let rs = false;
+                    for (const itask of tasks) {
+                        let task = listTasks.findItemById(itask);
+                        if (task) {
+                            task.isDone = !task.isDone;
+                            listTasks.updateItem(itask, task);
+                        }
+
+                        rs = true;
+                    }
+                    rs ? LoadPage(currentPage) : console.error("error");
                 }
                 break;
             }
